16326b7640b2f594d0c70e136e1c1e835004338f
Fering <treesortbubble@gmail.com>
2026-03-01 01:43:56 +0200

    feat: add merge videos by cross fade (#2)
    * wip
    
    * wip
    
    * refactor: поменять местами аргументы функции
    
    * wip: удалить лишнее

--- src/Script.fsx
+++ src/Script.fsx
@@ -26,9 +26,13 @@ module GenerateTextOptions =
             "-y"
             "-f lavfi"
             $"-i color=c=black:s={size}:d={options.DurationSeconds}"
-            $"-vf \"drawtext=text='{escapeText options.Text}':fontcolor=white:fontsize=48:x=(w-text_w)/2:y=(h-text_h)/2\""
+            "-f lavfi"
+            "-i anullsrc=channel_layout=stereo:sample_rate=44100"
+            $"-vf \"settb=1/15360,fps=30,drawtext=text='{escapeText options.Text}':fontcolor=white:fontsize=48:x=(w-text_w)/2:y=(h-text_h)/2\""
             "-c:v libx264"
             "-pix_fmt yuv420p"
+            "-c:a aac -b:a 128k"
+            "-shortest"
         ]
 
 let generateText (options: GenerateTextOptions) (output: string) =
@@ -39,11 +43,75 @@ let generateText (options: GenerateTextOptions) (output: string) =
         ]
     FfMpeg.startProc args
 
-// do
-//     "white_on_black.mp4"
-//     |> generateText {
-//         Size = 1368, 768
-//         DurationSeconds = 5
-//         Text = "It's so easy"
-//     }
-//     |> printfn "%A"
+type MergeCrossFadeOptions = {
+    /// Это то, за сколько секунд делается переход.
+    Duration: float32
+    /// Это то, с какой секунды начать переход.
+    Offset: float32
+}
+
+[<RequireQualifiedAccess>]
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module MergeCrossFadeOptions =
+    let toArgs (options: MergeCrossFadeOptions) =
+        let filter =
+            String.concat ";" [
+                $"[0:v][1:v]xfade=transition=fade:duration={options.Duration}:offset={options.Offset}[vid]"
+                "[0:a][1:a]acrossfade=duration=2[aud]"
+            ]
+        [
+            "-y"
+            $"-filter_complex \"{filter}\""
+            "-map \"[vid]\""
+            "-map \"[aud]\""
+        ]
+
+let mergeCrossFade (options: MergeCrossFadeOptions) (output: string) (input1: string, input2: string) =
+    let args =
+        String.concat " " [
+            $"-i {input1}"
+            $"-i {input2}"
+            yield! MergeCrossFadeOptions.toArgs options
+            output
+        ]
+    FfMpeg.startProc args
+
+let run (exitCode, stdout, stderr) =
+    printfn "%s" stdout
+    eprintfn "%s" stderr
+    exit exitCode
+
+let toResult (exitCode, stdout, stderr) =
+    match exitCode with
+    | 0 -> Ok ()
+    | _ -> Error (stdout, stderr)
+
+let exit =
+    function
+    | Ok () -> 0
+    | Error (stdout, stderr) ->
+        printfn "%s" stdout
+        eprintfn "%s" stderr
+        1
+    >> exit
+
+let createVideoWithHeader text outputVideoPath inputVideoPath =
+    let headerVideoPath = "headerVideoPath.mp4"
+    headerVideoPath
+    |> generateText {
+        Size = 1368, 768
+        DurationSeconds = 5
+        Text = text
+    }
+    |> toResult
+    |> Result.bind (fun () ->
+        (headerVideoPath, inputVideoPath)
+        |> mergeCrossFade
+            {
+                Duration = 2.5f
+                Offset = 2.5f
+            }
+            outputVideoPath
+        |> toResult
+    )
+    |> exit
