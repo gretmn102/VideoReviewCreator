4eca6207459dacb31e935109c1b95d039ba0672c
gretmn102 <treesortbubble@gmail.com>
2026-02-28 20:16:18 +0200

    wip

--- src/Script.fsx
+++ src/Script.fsx
@@ -39,11 +39,71 @@ let generateText (options: GenerateTextOptions) (output: string) =
         ]
     FfMpeg.startProc args
 
+type MergeCrossFadeOptions = {
+    /// Это то, за сколько секунд делается переход.
+    Duration: float32
+    /// Это то, с какой секунды начать переход.
+    Offset: float32
+}
+
+[<RequireQualifiedAccess>]
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module MergeCrossFadeOptions =
+    let toArgs (options: MergeCrossFadeOptions) =
+        let filter =
+            String.concat ";" [
+                "[0:v]format=yuv420p,setsar=1,fps=30[v0]"
+                "[1:v]format=yuv420p,setsar=1,fps=30[v1]"
+                $"[v0][v1]xfade=transition=fade:duration={options.Duration}:offset={options.Offset}[vid]"
+                "[0:a][1:a]acrossfade=duration=2[aud]"
+            ]
+        [
+            "-y"
+            $"-filter_complex \"{filter}\""
+            "-map \"[vid]\""
+            "-map \"[aud]\""
+            "-c:v libx264"
+            "-crf 23"
+            "-preset medium"
+        ]
+
+
+let mergeCrossFade (input1: string) (input2: string) (options: MergeCrossFadeOptions) (output: string) =
+    let args =
+        String.concat " " [
+            $"-i {input1}"
+            $"-i {input2}"
+            yield! MergeCrossFadeOptions.toArgs options
+            output
+        ]
+    FfMpeg.startProc args
+
+let run (exitCode, stdout, stderr) =
+    printfn "%s" stdout
+    eprintfn "%s" stderr
+    exit exitCode
+
 // do
-//     "white_on_black.mp4"
+//     "white_on_black1.mp4"
+//     |> generateText {
+//         Size = 1368, 768
+//         DurationSeconds = 5
+//         Text = "Hello, мир!"
+//     }
+//     |> run
+//     "white_on_black2.mp4"
 //     |> generateText {
 //         Size = 1368, 768
 //         DurationSeconds = 5
 //         Text = "It's so easy"
 //     }
-//     |> printfn "%A"
+//     |> run
+
+do
+    "result.mp4"
+    |> mergeCrossFade "white_on_black1.mp4" "/home/user/Videos/simplescreenrecorder-2026-02-28_04.37.05.mp4" {
+    // |> mergeCrossFade "white_on_black1.mp4" "white_on_black2.mp4" {
+        Duration = 2.5f
+        Offset = 2.5f
+    }
+    |> run
